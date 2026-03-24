8f2bb1ddc62857d1803b19497bbeba215575df0c
Aryan <aryangadhave202@gmail.com>
2026-03-24 20:32:20 +0530

    url_changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,21 @@ import chatRouter from "./routes/chat.routes.js";
 import cookieParser from "cookie-parser";
 import morgan from 'morgan'
 import cors from 'cors'
+import { fileURLToPath } from 'url'
+import path from 'path'
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url)
+console.log(__filename)
+const __dirname = path.dirname(__filename)
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json())
+app.use(express.static('./public')) 
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aether-chatbot.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
@@ -19,12 +28,11 @@ app.use(morgan("dev"))
 /*
 @ Health-check 
 */
-app.get("/",(req,res)=>{
-    res.send({
-        message:"server is running "
-    })
-})
 
 app.use(handleError);
 
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+    
 export default app;
--- frontend/src/features/auth/sockets/auth.socket.js
+++ frontend/src/features/auth/sockets/auth.socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socket = io("http://localhost:3000/", {
+export const socket = io("http://aether-chatbot.onrender.com/", {
     withCredentials: true,
     autoConnect: false
 })
